c16e09c41acbfc1cdd54aa5b52274c9ebbcd05ca
Udayakumar <tudaya.kumar@hotmail.com>
2024-02-07 09:52:22 +0530

    First commit of project setup files

--- myApp.js
+++ myApp.js
@@ -1,34 +1,108 @@
-var app = require('mongoose');
+var mongoose = require('mongoose');
+var MongoClient = require('mongodb').MongoClient;
+
+
 require('dotenv').config();
 let URI = process.env.MONGO_URI;
-app.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
-console.log('Mango DB Connected');
-let Person;
 
-const createAndSavePerson = (done) => {
-  done(null /*, data*/);
+MongoClient.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true } ,function(err, db) {
+  if (err) {
+    console.log('************** Exception in Connecting Mongo DB ************');
+    throw err;
+  }
+  console.log('************** Mongo DB connected ************');
+  db.close();
+});
+
+//mongoose.connect(URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+/*const connectDB = async () => {
+  try {
+    const conn = await mongoose.connect(`mongodb://localhost:27017/TEST.TESTDB`, {
+      useNewUrlParser: true, useUnifiedTopology: true 
+    });
+    console.log(`MongoDB Connected: {conn.connection.host}`);
+  } catch (error) {
+    console.error("Error in connecting Mongo DB ",error.message);
+    process.exit(1);
+  }
+}*/
+
+//console.log('Mango DB Connected');
+
+const { Schema } = mongoose;
+
+const personSchema = new Schema({
+  name:{
+    type: String,
+    required: true
+  },
+    age: Number,
+    favoriteFoods : [String]
+});
+
+let Person = mongoose.model('Person', personSchema);
+
+var createAndSavePerson = function(done) {
+  var personDetails = new Person({name: "UdayaKumar", age: 84, favoriteFoods: ["Rice", "Panner", "Mango"]});
+  console.log("Name : ",personDetails);
+
+  personDetails.save(function(err, data) {
+    if (err) return console.error(err);
+    done(null, data)
+  });
 };
 
-const createManyPeople = (arrayOfPeople, done) => {
-  done(null /*, data*/);
+var arrayOfPeople = [
+  {name: "Del", age: 30, favoriteFoods: ["Chicked65"]},
+  {name: "Ram", age: 29, favoriteFoods: ["curd Rice"]},
+  {name: "Mohammed", age: 30, favoriteFoods: ["Beef Briyani"]}
+];
+
+var createManyPeople = function(arrayOfPeople, done) {
+  Person.create(arrayOfPeople, function (errror, people) {
+    if (errror) return console.log(errror);
+    done(null, people);
+  });
 };
 
-const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+var findPeopleByName = function(personName, done) {
+  Person.find({name: personName}, function (err, personFound) {
+    if (err) return console.log(err);
+    done(null, personFound);
+  });
 };
 
-const findOneByFood = (food, done) => {
-  done(null /*, data*/);
+var findOneByFood = function(food, done) {
+  Person.findOne({favoriteFoods: food}, function (err, person) {
+    if (err) return console.log(err);
+    done(null, person);
+  });
 };
 
-const findPersonById = (personId, done) => {
-  done(null /*, data*/);
+var findPersonById = function(personId, done) {
+  Person.findById(personId, function (err, data) {
+    if (err) return console.log(err);
+    done(null, data);
+  });
 };
 
 const findEditThenSave = (personId, done) => {
-  const foodToAdd = "hamburger";
+  const foodToAdd = 'Idly';
 
-  done(null /*, data*/);
+  // .findById() method to find a person by _id with the parameter personId as search key. 
+  Person.findById(personId, (err, person) => {
+    if(err) return console.log(err); 
+  
+    // Array.push() method to add "hamburger" to the list of the person's favoriteFoods
+    person.favoriteFoods.push(foodToAdd);
+
+    // and inside the find callback - save() the updated Person.
+    person.save((err, updatedPersonValue) => {
+      if(err) return console.log(err);
+      done(null, updatedPersonValue)
+    })
+  })
 };
 
 const findAndUpdate = (personName, done) => {
